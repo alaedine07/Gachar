6cf22a9391f4d11fad98c86513cf32e07c56dd5a
alaedine <alaedineboufaden@gmail.com>
2024-02-29 15:43:26 +0100

    handle sign in and sign up

---
+++ src/users/auth.service.ts
@@ -0,0 +1,43 @@
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+    constructor(private usersService: UsersService) {}
+
+    async signUp(email: string, password: string) {
+        // see if email is already used
+        const users = await this.usersService.find(email);
+        if (users.length) {
+            throw new BadRequestException('this email is already in use');
+        }
+
+        // hash user password
+        const salt = randomBytes(8).toString('hex');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        const result = salt + '.' + hash.toString('hex');
+
+        // create a new user and save it
+        const user = await this.usersService.create(email, result);
+
+        // return the new user
+        return user;
+    }
+
+    async signIn(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+        if(!user) {
+            throw new NotFoundException('user not found');
+        }
+        const [salt, storedHash] = user.password.split('.');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        if (storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('invalid password');
+        }
+        return user;
+    }
+}
