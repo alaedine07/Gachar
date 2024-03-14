41c9f664f96ab96a6af66caccf2c13913b5bf23a
alaedine07 <alaedineboufaden@gmail.com>
2024-03-14 02:28:42 +0100

    handle basic authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,7 +6,8 @@ import {
     Patch,
     Param,
     Query,
-    Delete
+    Delete,
+    Session
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -24,13 +25,27 @@ export class UsersController {
     ) {}
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signUp(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signUp(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     @Post('/signin')
-    loginUser(@Body() body: CreateUserDto) {
-        return this.authService.signIn(body.email, body.password);
+    async loginUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signIn(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        return session.userId = null;
+    }
+
+    @Get('whoami')
+    whoAmi(@Session() session: any) {
+        return this.usersService.findOne(session.userId);
     }
 
     @Get('/:id')
